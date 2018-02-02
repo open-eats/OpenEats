f88f4981e7eb36bab9fe901a85736f87cfdf9263
Ryan Noelk <ryannoelk@gmail.com>
2018-02-01 19:30:31 -0500

    Update quick-start.py

--- quick-start.py
+++ quick-start.py
@@ -9,13 +9,13 @@ from subprocess import call, Popen, PIPE
 def update_image_tags(version=None):
     version = version if version is not None else 'latest'
     version = '''version: '2.3'
-  services:
-    api:
-      image: openeats/openeats-api:%s
-    web:
-      image: openeats/openeats-web:%s
-    nginx:
-      image: openeats/openeats-nginx:%s
+services:
+  api:
+    image: openeats/openeats-api:%s
+  web:
+    image: openeats/openeats-web:%s
+  nginx:
+    image: openeats/openeats-nginx:%s
 ''' % (version, version, version)
     with open('docker-prod.version.yml', 'w') as f:
         f.write(version)
